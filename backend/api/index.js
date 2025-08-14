4ba561538d29c0ed9f973272ad0f37c786a9aea5
SuperAbeih99 <abeihhamani24@gmail.com>
2025-08-14 04:33:27 +0000

    Verce-l

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,3 @@
-const serverless = require("serverless-http");
+// Export Express app directly as a Vercel Node function handler
 const app = require("../app");
-
-module.exports = serverless(app);
+module.exports = app;
